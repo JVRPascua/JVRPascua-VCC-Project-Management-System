a04921b7443728c877cba705d2ea486e6bcb6fe8
Vincent <pascuavincentjohn@gmail.com>
2023-03-11 16:16:37 +0800

    config files fixed for deployment

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import * as path from 'path';
+import { fileURLToPath } from 'url';
 import projectsRoutes from './routes/projects.js';
 import authRoutes from './routes/auth.js';
 import tasksRoutes from './routes/tasks.js';
@@ -10,12 +11,14 @@ import commentsRoutes from './routes/comments.js';
 
 const app = express();
 dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
-app.use(express.static(path.resolve(__dirname, "../build")));
+app.use(express.static(path.resolve(__dirname, "vcc-pms/build")));
 
 app.use('/projects', projectsRoutes);
 app.use('/auth', authRoutes);
@@ -23,11 +26,11 @@ app.use('/tasks', tasksRoutes);
 app.use('/comments', commentsRoutes);
 
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, '../build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'vcc-pms/build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(port || 5000, () => {
+app.listen(PORT || 5000, () => {
     console.log(`App running on port ${PORT}.`);
  });
