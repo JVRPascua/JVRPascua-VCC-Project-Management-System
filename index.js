aaa5892b825d69ac9661b17270cb97c6203c60bf
Vincent <pascuavincentjohn@gmail.com>
2023-03-11 14:54:45 +0800

    server files moved outside

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import * as path from 'path';
 import projectsRoutes from './routes/projects.js';
 import authRoutes from './routes/auth.js';
 import tasksRoutes from './routes/tasks.js';
@@ -14,17 +15,19 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
+app.use(express.static(path.resolve(__dirname, "../build")));
+
 app.use('/projects', projectsRoutes);
 app.use('/auth', authRoutes);
 app.use('/tasks', tasksRoutes);
 app.use('/comments', commentsRoutes);
 
-app.get('/', (req, res) => {
-    res.send('APP IS RUNNING.');
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
-const port = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.listen(port || 5000, () => {
-    console.log(`App running on port ${port}.`);
+    console.log(`App running on port ${PORT}.`);
  });
